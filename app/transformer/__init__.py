82e881e3317bb4f1ec15b120768731cecbfff572
Your Name <you@example.com>
2023-08-21 22:49:36 +0200

    Added first version of transformer module

---
+++ app/transformer/__init__.py
@@ -0,0 +1,2 @@
+from app.transformer.constant.JsonField import JsonField
+from app.transformer.service import JsonService
---
+++ app/transformer/service/JsonService.py
@@ -0,0 +1,5 @@
+import json
+
+
+def read_file_as_json(json_file):
+    return json.load(json_file)
